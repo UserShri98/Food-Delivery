808e70cdb23ab9b63c55cbce1e7959c35bee98bd
UserShri98 <sjc3098@gmail.com>
2025-05-29 13:14:38 +0530

    half accordion

--- food/src/components/ItemList.jsx
+++ food/src/components/ItemList.jsx
@@ -4,14 +4,31 @@ const ItemList=({items})=>{
     return (
         <div>
         {items.map((item)=>(
-            <div key={item.card.info.id} className="p-2 m-2 border-gray-200 border-b-2 text-left">
+            <div key={item.card.info.id} className="p-2 m-2 border-gray-200 border-b-2 text-left flex justify-between">
+
+
+           <div className="w-9/12">
+    
           <div className="py-2" > 
-            <span>{item.card.info.name}</span>    
+            <span>{item.card.info.name}-</span>    
             <span> 
   {item.card.info.price || item.card.info.defaultPrice/100}  ₹ /-</span>
           </div>
           <p className="text-xs">{item.card.info.description}</p>
+         </div>
+         <div className="w-5/12 p-4">
+         <div className="absolute">
+            <button className="p-2 mx-16 shadow-lg rounded-lg bg-black text-white">Add +</button>
+         </div>
+            {item.card.info.imageId && (
+  <img className="w-full"
+    src={`https://media-assets.swiggy.com/swiggy/image/upload/${item.card.info.imageId}`}
+    alt={item.card.info.name || "Food item"}
+  />
+)}
+            </div>
            </div>
+           
         ))}
         </div>
     )
